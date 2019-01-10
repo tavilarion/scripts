d2b02a4d876e095fdfeb9881bd2039ddbe445b2d
octavianlarion <tavilarion90@gmail.com>
2019-01-10 11:46:29 +0200

    Adds rebase with original branch.

--- git_helpers.sh
+++ git_helpers.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-# sync fork with original
+# sync fork with original using reset, assume remote name 'upstream'
 # uses git reset, don't use if you have unmerged commits
+function rst() {
+  if [ -z "$1" ]
+    then
+      echo "Must supply target branch."
+      return
+  fi
+  git checkout $1
+  git fetch upstream $1
+  git reset --hard upstream/$1
+}
+
+# rebase fork with original branch
+# assume remote name 'upstream'
 function rb() {
   if [ -z "$1" ]
     then
       echo "Must supply target branch."
       return
-  fi	
+  fi
   git checkout $1
   git fetch upstream $1
-  git reset --hard upstream/$1
+  git rebase upstream/$1
 }
 
